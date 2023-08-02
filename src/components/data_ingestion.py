115d3b504ac48cbb2e8092a0a28b862afd25c6a5
Harish <harishvarmapotturi@gmail.com>
2023-08-02 16:06:19 +0530

    Updated data ingestion file

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -43,7 +43,7 @@ class DataIngestion:
 
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            logging.info("Inmgestion of the data iss completed")
+            logging.info("Ingestion of the data is completed")
 
             return(
                 self.ingestion_config.train_data_path,
